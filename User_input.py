278339b5e268a365204dba9b096f52edbf1688f3
Melusi <160060286+Elgovernor@users.noreply.github.com>
2024-03-18 13:13:56 +0200

    Add files via upload

---
+++ User_input.py
@@ -0,0 +1,19 @@
+
+name = input("Please enter your name: ")
+
+
+print("Hello, " + name + "! Nice to meet you.")
+
+
+age = input("Please enter your age: ")
+
+
+print("You entered:", age)
+
+
+location = input("Please enter your location: ")
+
+
+print("Your location is:", location)
+
+
